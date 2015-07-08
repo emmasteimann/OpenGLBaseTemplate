88f2aee6ea26799ba0e3b9622806285279a35fc6
Emma Steimann <sakura@Sakura.local>
2015-07-08 11:44:41 -0500

    Initial Commit

---
+++ OpenGLBaseTemplate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLBaseTemplate
+//
+//  Created by Emma Steimann on 7/8/15.
+//  Copyright (c) 2015 Emma Steimann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
